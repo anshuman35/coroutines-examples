9867506b3f91ffb30d464b1ae59240d18622b180
anshuman35 <49508586+anshuman35@users.noreply.github.com>
2021-08-20 17:07:47 +0530

    Update channel-example-1.kt

--- examples/channel/channel-example-1.kt
+++ examples/channel/channel-example-1.kt
@@ -4,7 +4,13 @@ import channel.*
 import delay.*
 
 // https://tour.golang.org/concurrency/1
-
+ fun cxz(z:Int){
+    
+   for (i in 1..3){
+       println(z)
+       
+   }
+}
 suspend fun say(s: String) {
     for (i in 0..4) {
         delay(100)
@@ -12,8 +18,10 @@ suspend fun say(s: String) {
     }
 }
 
-fun main(args: Array<String>) = mainBlocking {
+fun main(args: Array<String>) = 
+    mainBlocking {
     go { say("world") }
     say("hello")
+    go{cxz(2)}
 }
 
